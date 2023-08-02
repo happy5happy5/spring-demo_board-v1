89d9ea1bedddb8a3f4e1c9dd0f69d091860f89de
happy5happy5 <zombil8731@gmail.com>
2023-08-02 16:56:10 +0900

    first commit

---
+++ src/test/java/com/msa/springdemo_boardv1/SpringDemoBoardV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msa.springdemo_boardv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoBoardV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
